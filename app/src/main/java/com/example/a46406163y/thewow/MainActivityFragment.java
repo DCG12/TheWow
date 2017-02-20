d685f1a476b42bb8f82a735907ce232214649359
David <46406163y@iespoblenou.org>
2017-02-20 19:17:51 +0100

    Creacio adapter i arrayList

--- app/src/main/java/com/example/a46406163y/thewow/MainActivityFragment.java
+++ app/src/main/java/com/example/a46406163y/thewow/MainActivityFragment.java
@@ -5,11 +5,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
 
     public MainActivityFragment() {
     }
@@ -17,6 +26,21 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView lvWow = (ListView) view.findViewById(R.id.lvWow);
+
+
+
+        items = new ArrayList<>();
+        adapter = new ArrayAdapter<>(
+                getContext(),
+                R.layout.lv_wow_row,
+                R.id.tvWow,
+                items
+        );
+        lvWow.setAdapter(adapter);
+
+        return view;
     }
 }
