9c36dfd5b41c1d354e4d7f5a7a3f4324e3b341af
David <46406163y@iespoblenou.org>
2017-02-20 19:20:56 +0100

    Creacio del POJO HttpUtils i del Apiwow

---
+++ app/src/main/java/com/example/a46406163y/thewow/WOW.java
@@ -0,0 +1,72 @@
+package com.example.a46406163y.thewow;
+
+
+public class WOW {
+    private String name;
+    private String description;
+    private int health;
+    private int level;
+    private int heroichealth;
+    private int heroiclevel;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHeroiclevel() {
+        return heroiclevel;
+    }
+
+    public void setHeroiclevel(int heroiclevel) {
+        this.heroiclevel = heroiclevel;
+    }
+
+    public int getHeroichealth() {
+        return heroichealth;
+    }
+
+    public void setHeroichealth(int heroichealth) {
+        this.heroichealth = heroichealth;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "WOW{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", health=" + health +
+                ", level=" + level +
+                ", heroichealth=" + heroichealth +
+                ", heroiclevel=" + heroiclevel +
+                '}';
+    }
+
+}
