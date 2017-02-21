3138449418a46139646b16a28d4b43549b29c491
David <46406163y@iespoblenou.org>
2017-02-21 17:32:45 +0100

    Definicio del OnItemClickListener i preparar dades al detail activity fragment

--- app/src/main/java/com/example/a46406163y/thewow/DetailActivityFragment.java
+++ app/src/main/java/com/example/a46406163y/thewow/DetailActivityFragment.java
@@ -1,7 +1,9 @@
 package com.example.a46406163y.thewow;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +16,25 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        Intent i = getActivity().getIntent();
+
+        if (i != null) {
+            WOW wow = (WOW) i.getSerializableExtra("wow");
+
+            if (wow != null) {
+                updateUi(wow);
+            }
+        }
+
+        return view;
+    }
+
+    private void updateUi(WOW wow) {
+        Log.d("BOSS", wow.toString());
+
+
     }
 
 }
--- app/src/main/java/com/example/a46406163y/thewow/MainActivityFragment.java
+++ app/src/main/java/com/example/a46406163y/thewow/MainActivityFragment.java
@@ -1,5 +1,6 @@
 package com.example.a46406163y.thewow;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
@@ -13,6 +14,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -45,15 +47,25 @@ public class MainActivityFragment extends Fragment {
 
         ListView lvWow = (ListView) view.findViewById(R.id.lvWow);
 
-
-
         items = new ArrayList<>();
         adapter = new WowAdapter(
                 getContext(),
                 R.layout.lv_wow_row,
                 items
         );
+
         lvWow.setAdapter(adapter);
+
+        lvWow.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                WOW wow = (WOW) adapterView.getItemAtPosition(i);
+            }
+        }
+
+        );
+
 
         return view;
     }
